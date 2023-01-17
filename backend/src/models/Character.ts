db82009d057c83f8f020f6eff64629fe281a05fe
MobinYardim <mobinyardim@gmail.com>
2023-01-18 03:12:06 +0330

    create domain model for character

--- backend/src/models/Character.ts
+++ backend/src/models/Character.ts
@@ -1,21 +1,43 @@
-type Alive = {
-    value: "Alive"
-}
-type Dead = {
-    value: "Dead"
-}
+import {Location} from "./Location";
+import {Origin} from "./Origin"
 
-type Unkown = {
-    value: "Unknown"
-}
-type Status = Alive | Dead | Unkown
+export type Status = "Alive" | "Dead" | "Unknown"
+export type Gender = "Female" | "Male" | "Genderless" | "unknown"
 
-class Character {
 
+export class Character {
+    name: string
     status: Status;
+    species: string
+    type: string
+    gender: Gender
+    origin: Origin
+    location: Location
+    image: string
+    episode: string[]
+    url: string
 
-    constructor(status: Status) {
-        this.status = status
+    constructor(
+        name: string,
+        status: Status,
+        species: string,
+        type: string,
+        gender: Gender,
+        origin: Origin,
+        location: Location,
+        image: string,
+        episode: string[],
+        url: string
+    ) {
+        this.name = name;
+        this.status = status;
+        this.species = species;
+        this.type = type;
+        this.gender = gender;
+        this.origin = origin;
+        this.location = location;
+        this.image = image;
+        this.episode = episode;
+        this.url = url;
     }
-
 }
