fd98d1daede677522befc2ed4cfbcac95f7c910e
MobinYardim <mobinyardim@gmail.com>
2023-01-19 14:42:50 +0330

    return characters with result model

--- backend/src/controller/Characters.ts
+++ backend/src/controller/Characters.ts
@@ -3,11 +3,18 @@ import {CharacterEntity} from "../persistence/CharacterEntity";
 import {RequestHandler} from "express";
 import {characterConverter} from "../converters/CharacterConverter";
 import {Document} from "mongoose";
+import {Success} from "../models/Result";
 
 export const getCharacters: RequestHandler = async (req, res) => {
     const characters = await CharacterDao.find().exec()
     const responseArray = characters.map((value: CharacterEntity & Document, index: number, array: CharacterEntity[]) => {
-        characterConverter.toDomain(value)
+        return characterConverter.toDomain(value)
     })
-    res.status(200).json(characters)
+    res.status(200).json(
+        new Success(
+            "Successful",
+            responseArray,
+            undefined
+        )
+    )
 }
