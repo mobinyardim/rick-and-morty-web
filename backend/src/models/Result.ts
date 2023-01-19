0df23c710f82498f03ad44016e79d0534c2c0867
MobinYardim <mobinyardim@gmail.com>
2023-01-19 14:42:15 +0330

    create result model class

---
+++ backend/src/models/Result.ts
@@ -0,0 +1,37 @@
+export class Success<Data> {
+    message: string
+    data: Data
+    pagination?: Pagination
+
+    constructor(
+        message: string,
+        data: Data,
+        pagination?: Pagination
+    ) {
+        this.message = message;
+        this.data = data;
+        this.pagination = pagination;
+    }
+
+}
+
+export class Pagination {
+    constructor(totalPages: number, totalCount: number) {
+        this.totalPages = totalPages;
+        this.totalCount = totalCount;
+    }
+
+    totalPages: number
+    totalCount: number
+}
+
+export class Fail {
+    message: string
+
+    constructor(message: string) {
+        this.message = message
+    }
+
+}
+
+export type Result = Success<any> | Fail;
