bb70a826240f4ec6242710c19f536e6fb0bdaa2a
MobinYardim <mobinyardim@gmail.com>
2023-01-19 13:34:07 +0330

    convert entity to domain model when return it

--- backend/src/controller/Characters.ts
+++ backend/src/controller/Characters.ts
@@ -1,7 +1,13 @@
-import CharacterEntity from "../models/CharacterEntity";
+import CharacterDao from "../persistence/CharacterEntity";
+import {CharacterEntity} from "../persistence/CharacterEntity";
 import {RequestHandler} from "express";
+import {characterConverter} from "../converters/CharacterConverter";
+import {Document} from "mongoose";
 
 export const getCharacters: RequestHandler = async (req, res) => {
-    const characters = await CharacterEntity.find().exec()
+    const characters = await CharacterDao.find().exec()
+    const responseArray = characters.map((value: CharacterEntity & Document, index: number, array: CharacterEntity[]) => {
+        characterConverter.toDomain(value)
+    })
     res.status(200).json(characters)
 }
