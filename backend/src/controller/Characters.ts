dcd9638b54d363f6ad88bd860b8bf3bdecb27bf8
MobinYardim <mobinyardim@gmail.com>
2023-01-20 12:10:15 +0330

    get pagination params from get characters api

--- backend/src/controller/Characters.ts
+++ backend/src/controller/Characters.ts
@@ -4,10 +4,13 @@ import {CharacterBody} from "../bodyModels/CharacterBody";
 import {services} from "../services/Services";
 import {Success} from "../models/Result";
 import {handleFailResult} from "../utils/ControllerHelpers";
+import {PaginationQueries} from "../bodyModels/PaginationQueries";
 
-export const getCharacters: RequestHandler = async (req, res) => {
+export const getCharacters: RequestHandler<unknown, unknown, unknown, PaginationQueries> = async (req, res) => {
+    const limit = req.query.limit ?? 10
+    const offset = req.query.offset ?? 0
 
-    const result = await services.characterService.getCharacters(10, 0)
+    const result = await services.characterService.getCharacters(limit, offset)
 
     if (result instanceof Success) {
         res.status(200).json(result)
