d4409a033a0c9338a1a56d95d17731a0012108b9
MobinYardim <mobinyardim@gmail.com>
2023-01-21 22:10:29 +0330

    add new error type NOT_VALID_INPUT

--- backend/src/models/Result.ts
+++ backend/src/models/Result.ts
@@ -25,7 +25,7 @@ export class Pagination {
     totalCount: number
 }
 
-export type ErrorType = "NOT_AUTHORIZED" | "READ_ERROR" | "UNKNOWN"
+export type ErrorType = "NOT_AUTHORIZED" | "NOT_VALID_INPUT" | "READ_ERROR" | "UNKNOWN"
 
 export class Fail {
     message: string
