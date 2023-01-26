61e78099aad4e8eb7939d62bd88d70cdae573681
MobinYardim <mobinyardim@gmail.com>
2023-01-26 11:26:55 +0330

    add all routes to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import MainScreen from "./screens/main/MainScreen";
+import LoginScreen from "./screens/auth/login/LoginScreen";
+import SignUpScreen from "./screens/auth/signup/SignUpScreen";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MainScreen/>}/>
+                <Route path="login" element={<LoginScreen/>}/>
+                <Route path="signup" element={<SignUpScreen/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
