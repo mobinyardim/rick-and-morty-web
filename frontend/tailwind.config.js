0dc7fb8dece8795eedb3cf3c194db05d39002b89
MobinYardim <mobinyardim@gmail.com>
2023-01-26 14:11:08 +0330

    add some needed colors to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,22 @@ module.exports = withMT({
     ],
     theme: {
         extend: {},
+        colors: {
+            primary: "#3B82F6",
+            onPrimary: "#EEEEEE",
+            primaryContainer: "#EEF7FE",
+            onPrimaryContainer: "#4189C3",
+
+            background: "#FFFFFF",
+            backgroundVariant: "#F8F9FC",
+            backgroundDisabled: "#F3F5F9",
+            divider: "#E7EBF3",
+            disabled: "#CFD7E6",
+            stroke: "#9FAECD",
+            onBackgroundHigh: "#262B36",
+            onBackgroundMedium: "#465064",
+            onBackgroundLow: "#677593"
+        }
     },
     plugins: [],
 })
