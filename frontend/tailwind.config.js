b99fc12796a00d71ac72ecededef5eabfef85872
MobinYardim <mobinyardim@gmail.com>
2023-01-26 11:25:01 +0330

    add material tailwind to tailwind config file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,11 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+module.exports = withMT({
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+})
