c476ba0eaddd7c3f1432a9fd8426447b6264ba71
MobinYardim <mobinyardim@gmail.com>
2023-01-29 09:16:23 +0330

    add login form to login screen

--- frontend/src/screens/auth/login/LoginScreen.tsx
+++ frontend/src/screens/auth/login/LoginScreen.tsx
@@ -1,11 +1,48 @@
 import AuthBannerComponent from "../componenets/AuthBannerComponent";
+import { Button } from "@material-tailwind/react";
+import { MyInput } from "../../../components/MyInput";
 
 function LoginScreen() {
-    return (
+  return (
+    <div className="flex flex-row">
+      <AuthBannerComponent className={"w-[30rem] lg-max:hidden"} />
 
-        <AuthBannerComponent className={"w-[30rem] lg-max:hidden"}/>
+      <LoginForm className="mx-auto w-fit" />
+    </div>
+  );
+}
 
-    )
+interface AuthFormProps {
+  className: string;
+}
+
+function LoginForm(props: AuthFormProps) {
+  const { className } = props;
+  return (
+    <div
+      className={`inline-flex h-screen flex-col items-center justify-center ${className}`}
+    >
+      <div className="flex flex-col items-center">
+        <MyInput className={"w-80"} label="Email" type="email" />
+
+        <div className="h-5" />
+
+        <MyInput className={"w-80"} label="Password" type="password" />
+
+        <div className="h-2" />
+
+        <div className="flex w-full flex-row justify-end text-primary">
+          <a className="font-sans text-xs font-thin" href="/forgot-password">
+            Forgot Password?
+          </a>
+        </div>
+
+        <div className="h-5" />
+
+        <Button className="w-full font-sans normal-case">Sign In</Button>
+      </div>
+    </div>
+  );
 }
 
 export default LoginScreen;
