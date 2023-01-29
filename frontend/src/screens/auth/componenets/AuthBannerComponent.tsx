4da309e6713a45aaef4a6b96996cf6eeff86b27b
MobinYardim <mobinyardim@gmail.com>
2023-01-29 11:18:00 +0330

    improve design

--- frontend/src/screens/auth/componenets/AuthBannerComponent.tsx
+++ frontend/src/screens/auth/componenets/AuthBannerComponent.tsx
@@ -9,17 +9,21 @@ function AuthBannerComponent(props: AuthBannerComponentProps) {
       className={`inline-flex h-screen w-full flex-col justify-evenly bg-primary/80 ${className}`}
     >
       <div className={"flex h-screen w-full flex-col justify-center"}>
-        <span className="my-6 mx-auto inline w-fit font-sans text-3xl font-normal text-onPrimary">
+        <span className="my-6 mx-auto inline w-fit font-sans text-2xl font-normal text-onPrimary">
           The Rick and Morty API
         </span>
 
-        <img className="mx-12" src={"assets/rick-and-morty-banner.png"}  alt="The Rick and morty wallpaper"/>
+        <img
+          className="mx-12"
+          src={"assets/rick-and-morty-banner.png"}
+          alt="The Rick and morty wallpaper"
+        />
       </div>
 
-      <div className="mb-5 ml-10 flex flex-row font-sans font-thin">
-        <span className="font-sans text-sm text-onPrimary">by&ensp;</span>
+      <div className="mb-5 ml-10 flex flex-row">
+        <span className="font-sans font-thin text-sm text-onPrimary">by&ensp;</span>
         <a
-          className=" font-sans text-sm text-onPrimary underline"
+          className=" font-sans font-thin text-sm text-onPrimary underline"
           href={"https://github.com/mobinyardim"}
         >
           Mobin Yardim
--- frontend/src/screens/auth/login/LoginScreen.tsx
+++ frontend/src/screens/auth/login/LoginScreen.tsx
@@ -1,6 +1,6 @@
 import AuthBannerComponent from "../componenets/AuthBannerComponent";
 import { MyInput } from "../../../components/MyInput";
-import {MyButton} from "../../../components/MyButton";
+import { MyButton } from "../../../components/MyButton";
 
 function LoginScreen() {
   return (
@@ -20,10 +20,19 @@ function LoginForm(props: AuthFormProps) {
   const { className } = props;
   return (
     <div
-      className={`inline-flex h-screen flex-col items-center justify-center ${className}`}
+      className={`inline-flex h-screen flex-col lg:w-full lg-max:justify-center ${className}`}
     >
-      <div className="flex flex-col items-center">
-        <MyInput className={"w-80"} label="Email" type="email" />
+      <div className="flex flex-col items-end lg-max:mt-5 lg:m-5 lg-max:order-1">
+        <span className="font-sans text-xs font-thin">
+          Not a member?&ensp;{" "}
+          <a className="font-sans text-xs font-thin text-primary" href="signIn">
+            Sign up now
+          </a>
+        </span>
+      </div>
+
+      <div className="flex w-fit flex-col items-center justify-center lg:mx-auto lg:my-auto">
+        <MyInput className={"inline w-80"} label="Email" type="email" />
 
         <div className="h-5" />
 
@@ -39,7 +48,7 @@ function LoginForm(props: AuthFormProps) {
 
         <div className="h-5" />
 
-        <MyButton className="w-full font-sans normal-case">Sign In</MyButton>
+        <MyButton className="w-80 font-sans normal-case">Sign In</MyButton>
       </div>
     </div>
   );
