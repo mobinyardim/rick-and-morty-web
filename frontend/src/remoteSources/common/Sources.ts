2a30443d49ea9866793fdcb9d851aa303d5d1058
mobinyardim <mobinyardim@gmail.com>
2023-02-11 13:20:44 +0330

    create new sources object to hold remote sources

---
+++ frontend/src/remoteSources/common/Sources.ts
@@ -0,0 +1,6 @@
+import CharactersRemoteSource from "../charactersRemoteSource/CharactersRemoteSource";
+import {CharactersRemoteSourceImpl} from "../charactersRemoteSource/CharactersRemoteSourceImpl";
+
+export const sources = {
+    charactersSource: new CharactersRemoteSourceImpl() as CharactersRemoteSource
+}
