856b60d465e9d4c61af112eaf72196958cf5abad
mobinyardim <mobinyardim@gmail.com>
2023-02-11 11:30:10 +0330

    create new interface for characters

---
+++ frontend/src/remoteSources/CharactersRemoteSource.ts
@@ -0,0 +1,14 @@
+import {Character} from "models/src/Character"
+
+interface CharactersRemoteSource {
+
+    getCharacters(): Promise<Array<Character>>
+
+    getCharacter(id: string): Promise<Character>
+
+    addCharacter(character: Omit<Character, "id">): Promise<Character>
+
+
+    updateCharacter(id: string, character: Partial<Character>): Promise<Character>
+
+}
