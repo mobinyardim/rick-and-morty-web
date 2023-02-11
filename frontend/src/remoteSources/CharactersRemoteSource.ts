2dedbb3eb9b2b5ed035ac698019eab0e80bc451a
mobinyardim <mobinyardim@gmail.com>
2023-02-11 11:32:47 +0330

    export default

--- frontend/src/remoteSources/CharactersRemoteSource.ts
+++ frontend/src/remoteSources/CharactersRemoteSource.ts
@@ -2,13 +2,15 @@ import {Character} from "models/src/Character"
 
 interface CharactersRemoteSource {
 
-    getCharacters(): Promise<Array<Character>>
+     getCharacters(): Promise<Array<Character>>
 
-    getCharacter(id: string): Promise<Character>
+     getCharacter(id: string): Promise<Character>
 
-    addCharacter(character: Omit<Character, "id">): Promise<Character>
+     addCharacter(character: Omit<Character, "id">): Promise<Character>
 
 
-    updateCharacter(id: string, character: Partial<Character>): Promise<Character>
+     updateCharacter(id: string, character: Partial<Character>): Promise<Character>
 
-}
+ }
+
+export default CharactersRemoteSource;
