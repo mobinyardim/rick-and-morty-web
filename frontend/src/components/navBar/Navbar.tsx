4b9fd32cec292c486be3cbe5b3efcead4c4115db
mobinyardim <mobinyardim@gmail.com>
2023-02-12 14:33:34 +0330

    improve navbar design

--- frontend/src/components/navBar/Navbar.tsx
+++ frontend/src/components/navBar/Navbar.tsx
@@ -1,138 +1,142 @@
-import { ElementType, useState } from "react";
+import {ElementType, useState} from "react";
 import * as FaIcons from "react-icons/fa";
 import * as MdIcon from "react-icons/md";
-import { IconBaseProps } from "react-icons/lib/esm/iconBase";
+import {IconBaseProps} from "react-icons/lib/esm/iconBase";
 
 export interface NavItem {
-  title: string;
-  className?: string;
-  icon: ElementType<IconBaseProps>;
+    title: string;
+    className?: string;
+    icon: ElementType<IconBaseProps>;
 }
 
 export interface NavBarProps {
-  className?: string;
-  items?: Array<NavItem>;
-  onSelect?: (navItem: NavItem) => void;
+    className?: string;
+    items?: Array<NavItem>;
+    onSelect?: (navItem: NavItem) => void;
 }
 
-export function Navbar({ className, items, onSelect }: NavBarProps) {
-  const [sidebar, setSidebar] = useState(false);
+export function Navbar({className, items, onSelect}: NavBarProps) {
+    const [sidebar, setSidebar] = useState(false);
 
-  const toggleSidebarViewState = () => setSidebar(!sidebar);
+    const toggleSidebarViewState = () => setSidebar(!sidebar);
 
-  return (
-    <>
-      <div
-        className={`flex h-16 items-center justify-start bg-background ${className}`}
-      >
-        <FaIcons.FaBars
-          className="h-full w-16 p-5"
-          onClick={toggleSidebarViewState}
-        />
-      </div>
-
-      <nav
-        className={`fixed top-0 z-10 flex h-screen w-72 transform-gpu flex-col rounded-r-2xl bg-background drop-shadow-md duration-500 ${
-          sidebar ? "left-0" : "-left-full"
-        }`}
-      >
-        <div className="absolute flex w-full flex-row justify-end">
-          <div
-            className="relative left-10 h-20 w-20  p-6"
-            onClick={toggleSidebarViewState}
-          >
-            <div className="z-10 m-auto h-8 w-8 rounded-full bg-background drop-shadow-2xl">
-              <MdIcon.MdOutlineKeyboardArrowRight
-                className={`m-auto h-8 w-8 transform-gpu duration-500 ${
-                  sidebar ? "rotate-180" : ""
-                }`}
-              ></MdIcon.MdOutlineKeyboardArrowRight>
+    return (
+        <>
+            <div
+                className={`flex h-16 items-center justify-start bg-background ${className}`}
+            >
+                <FaIcons.FaBars
+                    className="h-full w-16 p-5"
+                    onClick={toggleSidebarViewState}
+                />
             </div>
-          </div>
-        </div>
 
-        <NavItems className={"mt-20"} items={items} onSelect={onSelect} />
-      </nav>
-    </>
-  );
+            <nav
+                className={`fixed top-0 z-10 flex h-screen w-72 max-lg:w-72 ${sidebar ? "lg:w-72" : "lg:w-24"} transform-gpu flex-col rounded-r-2xl bg-background drop-shadow-md duration-500 ${
+                    sidebar ? "left-0" : "lg-max:-left-full"
+                }`}
+            >
+                <div className="absolute flex w-full flex-row justify-end">
+                    <div
+                        className={`relative left-10 h-20 w-20 p-6`}
+                        onClick={toggleSidebarViewState}
+                    >
+                        <div className="z-10 m-auto h-8 w-8 rounded-full bg-background drop-shadow-2xl">
+                            <MdIcon.MdOutlineKeyboardArrowRight
+                                className={`m-auto h-8 w-8 transform-gpu duration-500 ${
+                                    sidebar ? "rotate-180" : ""
+                                }`}
+                            ></MdIcon.MdOutlineKeyboardArrowRight>
+                        </div>
+                    </div>
+                </div>
+
+                <NavItems className={"mt-20"} items={items} onSelect={onSelect} isFull={sidebar}/>
+            </nav>
+        </>
+    );
 }
 
 interface NavItemsProps {
-  className?: string;
-  items?: Array<NavItem>;
-  onSelect?: (navItem: NavItem) => void;
+    className?: string;
+    isFull?: boolean;
+    items?: Array<NavItem>;
+    onSelect?: (navItem: NavItem) => void;
 }
 
-function NavItems({ className, items, onSelect }: NavItemsProps) {
-  const [selectedBackgroundTranslate, setSelectedBackgroundTranslate] =
-    useState("translate-y-0");
+function NavItems({className, isFull, items, onSelect}: NavItemsProps) {
+    const [selectedBackgroundTranslate, setSelectedBackgroundTranslate] =
+        useState("translate-y-0");
 
-  function handleSelectedItem(item: NavItem, index: number) {
-    document.documentElement.style.setProperty(
-      "--menuItemSize",
-      `${index * 4}rem`
+    function handleSelectedItem(item: NavItem, index: number) {
+        document.documentElement.style.setProperty(
+            "--menuItemSize",
+            `${index * 4}rem`
+        );
+        onSelect?.(item);
+        setSelectedBackgroundTranslate(`translate-y-[var(--menuItemSize)] `);
+    }
+
+    return (
+        <div className={`${className}`}>
+            {items && (
+                <SelectedNavMenuItemBackground
+                    className={`transition-50 absolute w-full transform transform-gpu transition  ${selectedBackgroundTranslate}`}
+                />
+            )}
+            <ul className={`gap-4`}>
+                {items?.map((item, index) => {
+                    return (
+                        <li
+                            onClick={() => handleSelectedItem(item, index)}
+                            key={item.title}
+                        >
+                            <NavMenuItem name={item.title} Icon={item.icon} isFull={isFull}/>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
     );
-    onSelect?.(item);
-    setSelectedBackgroundTranslate(`translate-y-[var(--menuItemSize)] `);
-  }
-
-  return (
-    <div className={`${className}`}>
-      {items && (
-        <SelectedNavMenuItemBackground
-          className={`transition-50 absolute w-full transform transform-gpu transition  ${selectedBackgroundTranslate}`}
-        />
-      )}
-      <ul className={`gap-4`}>
-        {items?.map((item, index) => {
-          return (
-            <li
-              onClick={() => handleSelectedItem(item, index)}
-              key={item.title}
-            >
-              <NavMenuItem name={item.title} Icon={item.icon} />
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
 }
 
 interface NavMenuItemProps {
-  name: string;
-  Icon: ElementType<IconBaseProps>;
-  onClick?: VoidFunction;
+    name: string;
+    isFull?: boolean;
+    Icon: ElementType<IconBaseProps>;
+    onClick?: VoidFunction;
 }
 
-function NavMenuItem({ name, Icon, onClick }: NavMenuItemProps) {
-  return (
-    <div
-      className={`z-10 flex h-16 cursor-pointer flex-row justify-evenly gap-4 gap-4 bg-transparent pr-4 ring-transparent hover:bg-transparent`}
-      onClick={onClick}
-    >
-      <div className={`h-16 w-1`} />
-      <div className={`z-10 flex h-16 grow flex-row`}>
-        <Icon className={"h-16 w-16 p-3"} />
-        <span className={"my-auto w-fit justify-center"}>{name}</span>
-      </div>
-    </div>
-  );
+export function NavMenuItem({name, isFull, Icon, onClick}: NavMenuItemProps) {
+    return (
+        <div
+            className={`z-10 flex h-16 cursor-pointer overflow-clip flex-row justify-evenly flex-nowrap bg-transparent ring-transparent hover:bg-transparent`}
+            onClick={onClick}
+        >
+            <div className={`h-16 w-1 shrink-0`}/>
+            <div className={'w-4 shrink-0'}/>
+            <div className={`flex min-w-0 grow shrink flex-shrink h-16 flex-row`}>
+                <Icon className={"h-16 w-16 shrink-0 p-3"}/>
+                <span
+                    className={`my-auto min-w-0 justify-center w-fit whitespace-nowrap ${isFull ? "lg:visible lg:w-fit" : "lg:invisible lg:w-0"} delay-500 transition overflow-clip ease-in-out`}>{name}</span>
+            </div>
+        </div>
+    );
 }
 
 interface SelectedNavMenuItemBackgroundProps {
-  className?: string;
+    className?: string;
 }
 
 function SelectedNavMenuItemBackground(
-  props: SelectedNavMenuItemBackgroundProps
+    props: SelectedNavMenuItemBackgroundProps
 ) {
-  return (
-    <div
-      className={`flex h-16 flex-row justify-evenly gap-4 pr-4 ${props.className}`}
-    >
-      <div className={`h-16 w-1 rounded-r bg-primary`} />
-      <div className={` h-16 grow rounded-xl bg-primary/10`}></div>
-    </div>
-  );
+    return (
+        <div
+            className={`flex h-16 flex-row justify-evenly gap-4 pr-4 ${props.className}`}
+        >
+            <div className={`h-16 w-1 rounded-r bg-primary`}/>
+            <div className={` h-16 grow rounded-xl bg-primary/10`}></div>
+        </div>
+    );
 }
