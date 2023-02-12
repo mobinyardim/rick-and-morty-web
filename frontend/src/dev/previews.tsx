226416759281944ec471bf4601b46e58f02d99a1
mobinyardim <mobinyardim@gmail.com>
2023-02-12 19:25:49 +0330

    add character componenet to preview

--- frontend/src/dev/previews.tsx
+++ frontend/src/dev/previews.tsx
@@ -10,6 +10,10 @@ import {HomeScreen} from "../screens/main/home/HomeScreen";
 import {Navbar, NavItem, NavMenuItem} from "../components/navBar/Navbar";
 import MainScreen from "../screens/main/MainScreen";
 import * as MdIcon from "react-icons/md";
+import {CharacterComponent} from "../components/CharacterComponent";
+import {Character} from "models/src/Character"
+import {Origin} from "models/src/Origin"
+import {Location} from "models/src/Location"
 
 const menuItems: Array<NavItem> = [
     {
@@ -33,6 +37,78 @@ const menuItems: Array<NavItem> = [
         icon: MdIcon.MdOutlineKeyboardArrowRight,
     },
 ];
+
+const rick = new Character(
+    "1",
+    "Rick Sanchez",
+    "Alive",
+    "Human",
+    "Male",
+    new Origin(
+        "Earth (C-137)",
+        "https://rickandmortyapi.com/api/location/1"
+    ),
+    new Location(
+        "Citadel of Ricks",
+        "https://rickandmortyapi.com/api/location/3"
+    ),
+    "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    [
+        "https://rickandmortyapi.com/api/episode/1",
+        "https://rickandmortyapi.com/api/episode/2",
+        "https://rickandmortyapi.com/api/episode/3",
+        "https://rickandmortyapi.com/api/episode/4",
+        "https://rickandmortyapi.com/api/episode/5",
+        "https://rickandmortyapi.com/api/episode/6",
+        "https://rickandmortyapi.com/api/episode/7",
+        "https://rickandmortyapi.com/api/episode/8",
+        "https://rickandmortyapi.com/api/episode/9",
+        "https://rickandmortyapi.com/api/episode/10",
+        "https://rickandmortyapi.com/api/episode/11",
+        "https://rickandmortyapi.com/api/episode/12",
+        "https://rickandmortyapi.com/api/episode/13",
+        "https://rickandmortyapi.com/api/episode/14",
+        "https://rickandmortyapi.com/api/episode/15",
+        "https://rickandmortyapi.com/api/episode/16",
+        "https://rickandmortyapi.com/api/episode/17",
+        "https://rickandmortyapi.com/api/episode/18",
+        "https://rickandmortyapi.com/api/episode/19",
+        "https://rickandmortyapi.com/api/episode/20",
+        "https://rickandmortyapi.com/api/episode/21",
+        "https://rickandmortyapi.com/api/episode/22",
+        "https://rickandmortyapi.com/api/episode/23",
+        "https://rickandmortyapi.com/api/episode/24",
+        "https://rickandmortyapi.com/api/episode/25",
+        "https://rickandmortyapi.com/api/episode/26",
+        "https://rickandmortyapi.com/api/episode/27",
+        "https://rickandmortyapi.com/api/episode/28",
+        "https://rickandmortyapi.com/api/episode/29",
+        "https://rickandmortyapi.com/api/episode/30",
+        "https://rickandmortyapi.com/api/episode/31",
+        "https://rickandmortyapi.com/api/episode/32",
+        "https://rickandmortyapi.com/api/episode/33",
+        "https://rickandmortyapi.com/api/episode/34",
+        "https://rickandmortyapi.com/api/episode/35",
+        "https://rickandmortyapi.com/api/episode/36",
+        "https://rickandmortyapi.com/api/episode/37",
+        "https://rickandmortyapi.com/api/episode/38",
+        "https://rickandmortyapi.com/api/episode/39",
+        "https://rickandmortyapi.com/api/episode/40",
+        "https://rickandmortyapi.com/api/episode/41",
+        "https://rickandmortyapi.com/api/episode/42",
+        "https://rickandmortyapi.com/api/episode/43",
+        "https://rickandmortyapi.com/api/episode/44",
+        "https://rickandmortyapi.com/api/episode/45",
+        "https://rickandmortyapi.com/api/episode/46",
+        "https://rickandmortyapi.com/api/episode/47",
+        "https://rickandmortyapi.com/api/episode/48",
+        "https://rickandmortyapi.com/api/episode/49",
+        "https://rickandmortyapi.com/api/episode/50",
+        "https://rickandmortyapi.com/api/episode/51"
+    ],
+    "https://rickandmortyapi.com/api/character/1",
+    "Human"
+)
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
@@ -43,14 +119,14 @@ const ComponentPreviews = () => {
                 <AuthBannerComponent/>
             </ComponentPreview>
             <ComponentPreview path="/LoginScreen">
-        <LoginScreen />
-      </ComponentPreview>
-      <ComponentPreview path="/MyInput">
-        <MyInput />
-      </ComponentPreview>
-      <ComponentPreview path="/SignUpScreen">
-        <SignUpScreen />
-      </ComponentPreview>
+                <LoginScreen/>
+            </ComponentPreview>
+            <ComponentPreview path="/MyInput">
+                <MyInput/>
+            </ComponentPreview>
+            <ComponentPreview path="/SignUpScreen">
+                <SignUpScreen/>
+            </ComponentPreview>
             <ComponentPreview path="/HomeScreen">
                 <HomeScreen/>
             </ComponentPreview>
@@ -63,8 +139,14 @@ const ComponentPreviews = () => {
             <ComponentPreview path="/NavMenuItem">
                 <NavMenuItem name={"Item"} isFull={true} Icon={MdIcon.MdOutlineKeyboardArrowRight}/>
             </ComponentPreview>
+            <ComponentPreview path="/CharacterComponent">
+                <CharacterComponent character={rick}/>
+            </ComponentPreview>
+            <ComponentPreview path="/ComponentPreviews">
+                <ComponentPreviews/>
+            </ComponentPreview>
         </Previews>
-  );
+    );
 };
 
 export default ComponentPreviews;
