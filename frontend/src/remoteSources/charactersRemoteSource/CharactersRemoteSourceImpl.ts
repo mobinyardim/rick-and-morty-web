6ff8928e56d2f9c41ed87b15d0724b11a5dedd0d
mobinyardim <mobinyardim@gmail.com>
2023-02-13 11:57:04 +0330

    reformat codes

--- frontend/src/remoteSources/charactersRemoteSource/CharactersRemoteSourceImpl.ts
+++ frontend/src/remoteSources/charactersRemoteSource/CharactersRemoteSourceImpl.ts
@@ -1,34 +1,33 @@
 import CharactersRemoteSource from "./CharactersRemoteSource";
-import {Character} from "models/src/Character"
-import {BASE_URL} from "../common/Consts";
-import axios, {AxiosRequestConfig} from "axios";
-
+import { Character } from "models/src/Character";
+import { BASE_URL } from "../common/Consts";
+import axios, { AxiosRequestConfig } from "axios";
 
 export class CharactersRemoteSourceImpl implements CharactersRemoteSource {
+  async addCharacter(character: Omit<Character, "id">): Promise<Character> {
+    const options: AxiosRequestConfig<Omit<Character, "id">> = {
+      method: "POST",
+    };
 
+    return axios
+      .post<Character>(`${BASE_URL}/characters`, character, options)
+      .then((result) => {
+        return result.data;
+      });
+  }
 
-    async addCharacter(character: Omit<Character, "id">): Promise<Character> {
-        const options: AxiosRequestConfig<Omit<Character, "id">> = {
-            method: 'POST'
-        };
+  getCharacter(id: string): Promise<Character> {
+    throw Error();
+  }
 
-        return axios.post<Character>(`${BASE_URL}/characters`, character, options).then((result) => {
-            return result.data
-        })
-    }
+  getCharacters(): Promise<Array<Character>> {
+    throw Error();
+  }
 
-    getCharacter(id: string): Promise<Character> {
-        throw Error()
-
-    }
-
-    getCharacters(): Promise<Array<Character>> {
-        throw Error()
-
-    }
-
-    updateCharacter(id: string, character: Partial<Character>): Promise<Character> {
-        throw Error()
-    }
-
+  updateCharacter(
+    id: string,
+    character: Partial<Character>
+  ): Promise<Character> {
+    throw Error();
+  }
 }
