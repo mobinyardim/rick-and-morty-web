b3f3bd5bdffe80667838787d498c9661376ff9b2
mobinyardim <mobinyardim@gmail.com>
2023-02-13 11:55:27 +0330

    improve design

--- frontend/src/components/CharacterComponent.tsx
+++ frontend/src/components/CharacterComponent.tsx
@@ -1,48 +1,58 @@
-import {Character} from "models/src/Character";
-import {Typography} from "@material-tailwind/react";
+import { Character } from "models/src/Character";
+import { Typography } from "@material-tailwind/react";
 
 export interface CharacterComponentProps {
-    className?: string,
-    character: Character,
-
+  className?: string;
+  character: Character;
 }
 
-export function CharacterComponent(
-    {className, character}: CharacterComponentProps
-) {
-    let statusClassName = ""
-    switch (character.status) {
-        case "Alive": {
-            statusClassName = "bg-error"
-            break;
-        }
-        case "Dead": {
-            statusClassName = "bg-success"
-            break;
-        }
-        case "Unknown": {
-            statusClassName = "bg-onBackgroundMedium"
-            break;
-        }
+export function CharacterComponent({
+  className,
+  character,
+}: CharacterComponentProps) {
+  let statusClassName = "";
+  switch (character.status) {
+    case "Alive": {
+      statusClassName = "bg-error";
+      break;
     }
-    return (
-        <div className={`flex lg-max:flex-col lg:flex-row bg-surface w-full rounded-2xl overflow-clip ${className}`}>
-
-            <img src={character.image} className={`lg:w-48 lg:h-48  lg-max:aspect-square lg-max:grow`}/>
-            <div className={`flex flex-col p-4`}>
-                <Typography variant="h4" className={`text-onBackgroundHigh`}>{character.name}</Typography>
-                <div className={`flex flex-row gap-1 items-center`}>
-                    <div
-                        className={`w-3 h-3 aspect-square shrink-0 grow-0 ${statusClassName} rounded-full items-center`}/>
-                    <Typography variant="paragraph"
-                                className={`text-onBackgroundHigh`}>{character.status}&ensp;-&ensp;{character.species}</Typography>
-                </div>
-                <div className={`grow`}/>
-                <Typography variant="paragraph" className={`text-onBackgroundLow`}>Last known location:</Typography>
-                <Typography variant="paragraph"
-                            className={`text-onBackgroundHigh`}>{character.location.name}</Typography>
-
-            </div>
+    case "Dead": {
+      statusClassName = "bg-success";
+      break;
+    }
+    case "Unknown": {
+      statusClassName = "bg-onBackgroundMedium";
+      break;
+    }
+  }
+  return (
+    <div
+      className={`flex w-fit overflow-clip rounded-2xl bg-surface lg:w-96 lg:flex-row lg-max:flex-col ${className}`}
+    >
+      <img
+        src={character.image}
+        className={`lg:h-36 lg:w-36 lg-max:aspect-square  lg-max:w-60 lg-max:grow`}
+      />
+      <div className={`flex flex-col p-4`}>
+        <Typography variant="h5" className={`text-onBackgroundHigh`}>
+          {character.name}
+        </Typography>
+        <div className={`flex flex-row items-center gap-1`}>
+          <div
+            className={`aspect-square h-3 w-3 shrink-0 grow-0 ${statusClassName} items-center rounded-full`}
+          />
+          <Typography variant="paragraph" className={`text-onBackgroundHigh`}>
+            {character.status}&ensp;-&ensp;{character.species}
+          </Typography>
         </div>
-    );
+        <div className={`grow`} />
+        <Typography variant="paragraph" className={`text-onBackgroundLow`}>
+          Last known location:
+        </Typography>
+        <Typography variant="paragraph" className={`text-onBackgroundHigh`}>
+          {character.location.name}
+        </Typography>
+      </div>
+    </div>
+  );
 }
