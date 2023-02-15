446c11bbc07ad12b6688b0d408015653bab4d33d
mobinyardim <mobinyardim@gmail.com>
2023-02-15 10:40:57 +0330

    add some util functins for react router

---
+++ frontend/src/utils/ReactRouterUtils.ts
@@ -0,0 +1,36 @@
+// utils.ts
+import {
+  Await as RrdAwait,
+  defer,
+  LoaderFunctionArgs,
+  useLoaderData as useRrdLoaderData,
+} from "react-router-dom";
+
+export function useLoaderData<
+  TLoader extends ReturnType<typeof deferredLoader>
+>() {
+  return useRrdLoaderData() as ReturnType<TLoader>["data"];
+}
+
+export function deferredLoader<TData extends Record<string, unknown>>(
+  dataFunc: (args: LoaderFunctionArgs) => TData
+) {
+  return (args: LoaderFunctionArgs) =>
+    defer(dataFunc(args)) as Omit<ReturnType<typeof defer>, "data"> & {
+      data: TData;
+    };
+}
+
+export interface AwaitResolveRenderFunction<T> {
+  (data: Awaited<T>): React.ReactElement;
+}
+
+export interface AwaitProps<T> {
+  children: React.ReactNode | AwaitResolveRenderFunction<T>;
+  errorElement?: React.ReactNode;
+  resolve: Promise<T>;
+}
+
+export function Await<T>(props: AwaitProps<T>): JSX.Element {
+  return RrdAwait(props);
+}
