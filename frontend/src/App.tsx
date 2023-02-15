ddd2f916e4f3b828b45f8fad179a4fb897fe72f4
mobinyardim <mobinyardim@gmail.com>
2023-02-15 10:50:09 +0330

    create browser router instead of app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import LoginScreen from "./screens/auth/login/LoginScreen";
 import SignUpScreen from "./screens/auth/signup/SignUpScreen";
 import MainScreen from "./screens/main/MainScreen";
+import { sources } from "./remoteSources/common/Sources";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainScreen />} />
-        <Route path="login" element={<LoginScreen />} />
-        <Route path="signup" element={<SignUpScreen />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+export const appRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <Fragment>
+      <Route
+        path="/"
+        element={<MainScreen />}
+        loader={async () => {
+          return await sources.charactersSource.getCharacters();
+        }}
+      />
+      <Route path="login" element={<LoginScreen />} />
+      <Route path="signup" element={<SignUpScreen />} />
+    </Fragment>
+  )
+);
 
-export default App;
