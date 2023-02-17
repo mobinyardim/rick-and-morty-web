18ddd08cfa9ccc5b7ccfe5c4d16c43347cc1ba3b
mobinyardim <mobinyardim@gmail.com>
2023-02-17 13:19:50 +0330

    add on load next function to list items component

--- frontend/src/components/ItemsList.tsx
+++ frontend/src/components/ItemsList.tsx
@@ -1,12 +1,13 @@
 import { Typography } from "@material-tailwind/react";
 import * as BsIcon from "react-icons/bs";
-import React from "react";
+import React, { useEffect } from "react";
 
 interface ItemsListProp {
   className: string;
   title: string;
   isSeeMoreButtonVisible?: boolean;
   onSeeMore?: VoidFunction;
+  onLoadNext?: VoidFunction;
   items: JSX.Element[];
 }
 
@@ -16,7 +17,25 @@ export function ItemsList({
   title,
   onSeeMore,
   isSeeMoreButtonVisible,
+  onLoadNext,
 }: ItemsListProp) {
+  const handleScroll = (e: any) => {
+    if (
+      window.innerHeight + e.target.documentElement.scrollTop + 1 >=
+      e.target.documentElement.scrollHeight
+    ) {
+      onLoadNext?.();
+    }
+  };
+
+  useEffect(() => {
+    window?.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window?.removeEventListener("scroll", handleScroll, { capture: false });
+    };
+  });
+
   return (
     <div className={`flex flex-col ${className}`}>
       <div className={`flex max-w-[100vw] flex-row items-center px-8`}>
@@ -41,7 +60,7 @@ export function ItemsList({
       </div>
       <div className={`h-4 flex-shrink-0`} />
       <div
-        className={`grid h-fit w-fit max-w-[90vw] grid-flow-row grid-cols-const_40 justify-items-center gap-4 overflow-x-scroll lg:grid-cols-const_96 sm:lg-max:grid-cols-const_44`}
+        className={`grid h-fit w-fit max-w-[90vw] grid-flow-row grid-cols-const_40 justify-items-center gap-4 overflow-y-auto lg:grid-cols-const_96 sm:lg-max:grid-cols-const_44`}
       >
         {items.map((item) => item)}
       </div>
