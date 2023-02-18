a11e00e479c3a362a8eebbdf61834644873c73d2
MobinYardim <mobinyardim@gmail.com>
2023-02-19 01:31:12 +0330

    add last pagination field to character store

--- frontend/src/stores/CharctersStore.ts
+++ frontend/src/stores/CharctersStore.ts
@@ -1,15 +1,18 @@
 import { create } from "zustand";
 import { Character } from "models/src/Character";
+import { Pagination } from "models/src/Result";
 
 interface CharactersState {
   characters: Character[];
-  addCharacters: (newCharacters: Character[]) => void;
+  lastPagination?: Pagination;
+  addCharacters: (newCharacters: Character[],pagination?:Pagination) => void;
 }
 
 export const useCharactersStore = create<CharactersState>((set) => ({
   characters: new Array<Character>(),
-  addCharacters: (newCharacters: Character[]) =>
+  addCharacters: (newCharacters: Character[],pagination?:Pagination) =>
     set((state: CharactersState) => ({
       characters: [...state.characters, ...newCharacters],
+      lastPagination: pagination
     })),
 }));
