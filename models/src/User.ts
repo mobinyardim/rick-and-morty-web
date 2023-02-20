9084ebc5bb9595ec8d4ebdc14e7c0dcffe24533e
mobinyardim <mobinyardim@gmail.com>
2023-02-20 12:12:54 +0330

    add id to user object

--- models/src/User.ts
+++ models/src/User.ts
@@ -1,4 +1,5 @@
 export class User {
+  id: string;
   username: string;
 
   email: string;
@@ -8,11 +9,13 @@ export class User {
   avatar?: string;
 
   constructor(
+    id: string,
     username: string,
     email: string,
     isVerified: boolean,
     avatar?: string
   ) {
+    this.id = id;
     this.username = username;
     this.email = email;
     this.isVerified = isVerified;
