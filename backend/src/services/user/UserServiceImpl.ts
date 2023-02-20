cb1756572e44f430aa0cbb23e77acee39b0f7732
mobinyardim <mobinyardim@gmail.com>
2023-02-20 14:10:46 +0330

    implement login service

--- backend/src/services/user/UserServiceImpl.ts
+++ backend/src/services/user/UserServiceImpl.ts
@@ -5,24 +5,37 @@ import UserDao from "../../persistence/UserEntity";
 import { SignUpBody } from "models/src/bodyModels/SignUpBody";
 import bcrypt from "bcrypt";
 import { userConverter } from "../../converters/UserConverter";
+import { LoginBody } from "models/src/bodyModels/LoginBody";
 
 export class UserServiceImpl extends UserService {
   getUser(userId: string): Promise<Result<User>> {
     throw Error();
   }
 
-  login(): Promise<Result<User>> {
-    throw Error();
+  async login(loginBody: LoginBody): Promise<Result<User>> {
+    const { username, password } = loginBody;
+
+    const user = await UserDao.findOne({ username: username }).exec();
+    if (!user) {
+      return new Fail("Wrong credential!", 401, "NOT_AUTHORIZED");
+    }
+
+    const passwordHashed = await bcrypt.hash(password, 10);
+    if (passwordHashed != user.password) {
+      return new Fail("Wrong credential!", 401, "NOT_AUTHORIZED");
+    }
+
+    return new Success("Successful", userConverter.toDomain(user));
   }
 
   async signUp(signUpBody: SignUpBody): Promise<Result<User>> {
     const { username, password, email } = signUpBody;
 
-    if (await UserDao.findOne({ username: username }).exec()) {
+    if (!(await UserDao.findOne({ username: username }).exec())) {
       return new Fail("This username is taken before!", 400, "BAD_REQUEST");
     }
 
-    if (await UserDao.findOne({ email: email }).exec()) {
+    if (!(await UserDao.findOne({ email: email }).exec())) {
       return new Fail("This email is taken before!", 400, "BAD_REQUEST");
     }
 
