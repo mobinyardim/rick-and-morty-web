70600741bc9e4ab5f8576863b31aaf5bc31b2e88
mobinyardim <mobinyardim@gmail.com>
2023-02-20 14:18:26 +0330

    login bug fixes

--- backend/src/services/user/UserServiceImpl.ts
+++ backend/src/services/user/UserServiceImpl.ts
@@ -16,12 +16,12 @@ export class UserServiceImpl extends UserService {
     const { username, password } = loginBody;
 
     const user = await UserDao.findOne({ username: username }).exec();
-    if (!user) {
+    if (user === null) {
       return new Fail("Wrong credential!", 401, "NOT_AUTHORIZED");
     }
 
-    const passwordHashed = await bcrypt.hash(password, 10);
-    if (passwordHashed != user.password) {
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
       return new Fail("Wrong credential!", 401, "NOT_AUTHORIZED");
     }
 
@@ -31,11 +31,11 @@ export class UserServiceImpl extends UserService {
   async signUp(signUpBody: SignUpBody): Promise<Result<User>> {
     const { username, password, email } = signUpBody;
 
-    if (!(await UserDao.findOne({ username: username }).exec())) {
+    if (await UserDao.findOne({ username: username }).exec()) {
       return new Fail("This username is taken before!", 400, "BAD_REQUEST");
     }
 
-    if (!(await UserDao.findOne({ email: email }).exec())) {
+    if (await UserDao.findOne({ email: email }).exec()) {
       return new Fail("This email is taken before!", 400, "BAD_REQUEST");
     }
 
