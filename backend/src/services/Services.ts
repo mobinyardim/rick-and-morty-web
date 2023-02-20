e61d9c44b5ea744b9b9878e9a0f435db3006b13d
mobinyardim <mobinyardim@gmail.com>
2023-02-20 11:21:14 +0330

    add user service to services object

--- backend/src/services/Services.ts
+++ backend/src/services/Services.ts
@@ -1,6 +1,9 @@
 import { CharacterService } from "./character/CharacterService";
 import { CharacterServiceImpl } from "./character/CharacterServiceImpl";
+import { UserService } from "./user/UserService";
+import { UserServiceImpl } from "./user/UserServiceImpl";
 
 export const services = {
   characterService: <CharacterService>new CharacterServiceImpl(),
+  userService: <UserService>new UserServiceImpl(),
 };
