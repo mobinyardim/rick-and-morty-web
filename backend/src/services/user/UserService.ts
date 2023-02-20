229b4d9e6e609bc9ce87d4ba9a7aac4dbf18d67c
mobinyardim <mobinyardim@gmail.com>
2023-02-20 14:10:26 +0330

    addd login body to user service login function arguments

--- backend/src/services/user/UserService.ts
+++ backend/src/services/user/UserService.ts
@@ -1,11 +1,12 @@
 import { SignUpBody } from "models/src/bodyModels/SignUpBody";
+import { LoginBody } from "models/src/bodyModels/LoginBody";
 import { User } from "models/src/User";
 import { Result } from "../../../../models/src/Result";
 
 export abstract class UserService {
   abstract signUp(signUpBody: SignUpBody): Promise<Result<User>>;
 
-  abstract login(): Promise<Result<User>>;
+  abstract login(loginBody: LoginBody): Promise<Result<User>>;
 
   abstract getUser(userId: string): Promise<Result<User>>;
 }
