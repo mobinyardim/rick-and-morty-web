c30e7cc530a4d59fcd7bb22256b6cb22156993ad
mobinyardim <mobinyardim@gmail.com>
2023-02-20 14:18:51 +0330

    create login controller

--- backend/src/controller/User.ts
+++ backend/src/controller/User.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express";
 import { SignUpBody } from "models/src/bodyModels/SignUpBody";
+import { LoginBody } from "models/src/bodyModels/LoginBody";
 import { Success } from "models/src/Result";
 import { services } from "../services/Services";
 import { handleFailResult } from "../utils/ControllerHelpers";
@@ -11,6 +12,22 @@ export const signUp: RequestHandler<
   unknown
 > = async (req, res) => {
   const result = await services.userService.signUp(req.body);
+
+  if (result instanceof Success) {
+    req.session.user = result.data;
+    res.status(200).json(result);
+  } else {
+    handleFailResult(res, result);
+  }
+};
+
+export const login: RequestHandler<
+  unknown,
+  unknown,
+  LoginBody,
+  unknown
+> = async (req, res) => {
+  const result = await services.userService.login(req.body);
 
   if (result instanceof Success) {
     req.session.user = result.data;
