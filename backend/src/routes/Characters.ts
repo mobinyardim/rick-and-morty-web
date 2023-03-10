dba2fa78eb1caac7c1a2d2b1836ed47c8ce373d9
mobinyardim <mobinyardim@gmail.com>
2023-03-10 14:47:38 +0330

    add admin middle ware to some api s

--- backend/src/routes/Characters.ts
+++ backend/src/routes/Characters.ts
@@ -1,12 +1,26 @@
 import express from "express";
 import * as CharactersController from "../controller/Characters";
+import {
+  adminAuthMiddleWare,
+  userAuthMiddleWare,
+} from "../middlewares/AuthMiddleWare";
 
 export const router = express.Router();
 
 router.get("/", CharactersController.getCharacters);
 
-router.post("/populate", CharactersController.populateDatabase);
+router.post(
+  "/populate",
+  userAuthMiddleWare,
+  adminAuthMiddleWare,
+  CharactersController.populateDatabase
+);
 
 router.get("/:id", CharactersController.getCharacter);
 
-router.post("/", CharactersController.createCharacter);
+router.post(
+  "/",
+  userAuthMiddleWare,
+  adminAuthMiddleWare,
+  CharactersController.createCharacter
+);
