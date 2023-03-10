e4c88a3bdcb800b616a26d9cfd23a8ca9d2f2d0f
mobinyardim <mobinyardim@gmail.com>
2023-03-10 14:47:11 +0330

    create new middle ware for admin

--- backend/src/middlewares/AuthMiddleWare.ts
+++ backend/src/middlewares/AuthMiddleWare.ts
@@ -8,3 +8,15 @@ export const userAuthMiddleWare: RequestHandler = (req, res, next) => {
     res.status(401).json(new Fail("Not authorized", 401, "NOT_AUTHORIZED"));
   }
 };
+
+export const adminAuthMiddleWare: RequestHandler = (req, res, next) => {
+  if (req.session.user && req.session.user.isAdmin) {
+    next();
+  } else if (!req.session.user) {
+    res.status(401).json(new Fail("Not authorized", 401, "NOT_AUTHORIZED"));
+  } else {
+    res
+      .status(403)
+      .json(new Fail("Permission denied", 403, "PERMISSION_DENIED"));
+  }
+};
